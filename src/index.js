1bed7fcecb15a2ac3d086bbf213785bec3ea99e2
Ethan Miles <ethanmilescs@gmail.com>
2022-02-08 17:24:24 -0600

    completed 3 more event listeners, total is now 6

--- src/index.js
+++ src/index.js
@@ -3,27 +3,25 @@ import './less/index.less'
 // Your code goes here!
 //DOM
 const body = document.body;
-const imgButton = document.querySelector(' img')
+const imgButton = document.querySelector(' img');
 // const h2 = document.querySelectorAll('h2')
-const navElements = document.querySelectorAll('.nav-link')
+const navElements = document.querySelectorAll('.nav-link');
+const text = document.querySelector('h2')
 
 
 
 //Events 
-
+//1 Page Load
 window.onload = (event) => {
     alert('Just a warning: the font on this webpage is horrendous.');
     const heading = document.querySelector('h1')
     heading.textContent = 'Are you ready to be an instagram influencer?'
-    // body.classList.remove('color-shift');
-    // body.classList.remove('font-change');
-    // h2.classList.remove('font-change')
 }
-
+//2 Click
 imgButton.addEventListener('click', (event) => {
     console.log('This is an image')
 })
-
+//3 Mouseover 
 navElements.forEach(nav => {
    nav.addEventListener('mouseenter', function(event) {
        event.target.style.color = '#4BA0B5';
@@ -32,10 +30,22 @@ navElements.forEach(nav => {
       }, 500);
     }, false);
 })
+//4Keydown
+function escKey (event) {
+    if (event.key === 'Escape'){
+        document.body.style.backgroundColor = '#EAD1BB'
+    }
+}
+document.addEventListener('keydown', escKey)
+//5 Double Click
+text.addEventListener('dblclick', function(event)  {
+    event.target.style.color = '#4BA0B5';
+} )
+//6 Copy 
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText() 
+    .then(text => {
+       console.log(text)
+    })
+})
 
-// Array.from(document.links).forEach(link => {
-//     link.addEventListener('click', function(evt) {
-//         evt.preventDefault();
-//         console.log(`The ${evt.target.textContent} link is a crypto SCAM!`)
-//     })
-// })
